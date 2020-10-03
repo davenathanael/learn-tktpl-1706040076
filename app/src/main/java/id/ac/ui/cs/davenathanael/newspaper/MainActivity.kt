f80d3eddec8ef9f46c25a5b9bd86a2a5d00f5c38
Dave Nathanael <davenathanaeldev@gmail.com>
2020-10-03 23:46:35 +0700

    feat: implement back button override
    showing back button press count and a toast instead of finishing the activity

--- app/src/main/java/id/ac/ui/cs/davenathanael/newspaper/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/davenathanael/newspaper/MainActivity.kt
@@ -2,10 +2,22 @@ package id.ac.ui.cs.davenathanael.newspaper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var backCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onBackPressed() {
+        backCount += 1
+        val centerText: TextView = findViewById(R.id.center_text)
+        centerText.text = "Back x$backCount"
+        
+        Toast.makeText(applicationContext, "You pressed back!", Toast.LENGTH_SHORT).show()
     }
 }
